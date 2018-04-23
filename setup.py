36cb0837de20b4c408b24cbca0b6263a4d69d679
isezen <sezenismail@gmail.com>
2018-04-23 22:10:57 +0300

    init and setup files

---
+++ setup.py
@@ -0,0 +1,56 @@
+"""
+XtraBufr
+~~~~~~~~~
+Extra functions and tools to work with BUFR files. (Depends on ECMWF ecCodes)
+"""
+import os
+from setuptools import setup
+import fastentrypoints
+
+
+def get(x):
+    with open(os.path.join(os.path.dirname(__file__),
+                           'xtrabufr', '__init__.py')) as f:
+        for line in f.readlines():
+            if line.startswith('__' + x + '__'):
+                return line.split('=')[1].strip()[1:-1]
+
+
+def get_requirements():
+    requirements = ['numpy']
+    return requirements
+
+
+setup(
+    name='XtraBufr',
+    version=get('version'),
+    platforms=['linux', 'darwin'],
+    packages=['xtrabufr'],
+    package_dir={'xtrabufr': 'xtrabufr'},
+    include_package_data=True,
+    setup_requires=['pytest-runner'],
+    install_requires=get_requirements(),
+    tests_require=['pytest'],
+    scripts=['bin/xbsort', 'bin/xbsplit', 'bin/xbcp2bin'],
+    entry_points={
+        'console_scripts': ['xbdef = xtrabufr._scripts_:_xbdef_',
+                            'xbcopy = xtrabufr._scripts_:_xbcopy_'],
+    },
+    author=get('author'),
+    author_email=get('email'),
+    description='Extra BUFR tools (build on ecCodes) to work with BUFR files.',
+    long_description=__doc__,
+    license=get('license'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS dependent",
+        "License :: OSI Approved :: AGPL v3.0 License",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities",
+    ],
+    keywords=['BUFR', 'WMO', 'ecCodes'],
+    url='https://github.com/isezen/xtrabufr',
+)
---
+++ xtrabufr/__init__.py
@@ -0,0 +1,24 @@
+"""
+XtraBufr
+~~~~~~~~~
+Process WMO BUFR messages. (Depends on ECMWF ecCodes)
+
+This package contains extra functions and tools to work with
+ BUFR files.
+
+:github: https://github.com/isezen/xtrabufr
+:docs: https://github.com/isezen/xtrabufr
+:author: Ismail SEZEN (sezenismail@gmail.com)
+"""
+from __future__ import absolute_import
+from .__extra__ import *
+from . import definitions
+from . import objects
+
+
+__name__ = 'XtraBufr'
+__package__ = 'xtrabufr'
+__version__ = '0.0.1.dev'
+__author__ = 'Ismail SEZEN'
+__email__ = 'sezenismail@gmail.com'
+__license__ = 'AGPL v3.0'
